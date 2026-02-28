86155baaa59bdf42ac7d6c12725855108e6241e8
Frank Möhle <91616163+softhack007@users.noreply.github.com>
2026-02-28 19:57:13 +0100

    Remove duplicate TAG declaration in NeoEsp32RmtXMethod.h
    fixes a compile error that was introduced by the latest "Merge branch 'Makuna:CORE3'"

--- src/internal/methods/ESP/ESP32/NeoEsp32RmtXMethod.h
+++ src/internal/methods/ESP/ESP32/NeoEsp32RmtXMethod.h
@@ -30,7 +30,6 @@ License along with NeoPixel.  If not, see
 
 // Needed for ESP_* macros that take a log_tag
 #include "esp_log.h"
-static const char* TAG = "NeoEsp32RmtX";
 
 #include <Arduino.h>
 #include "NeoEsp32RmtSpeed.h"
